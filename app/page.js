3a30da98afccf5b53d3d7e4827e75e0465515fba
Mitinul <torabi9030@gmail.com>
2024-08-02 11:57:44 +0330

    Add h1 tag

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
+import { Typography } from "@mui/material";
+
 export default function Home() {
   return (
     <main>
-      hello
+      <Typography
+        variant="h1"
+        color="secondary"
+        fontSize={{ xs: 50, sm: 96 }}
+        fontWeight={600}
+      >
+        Perfect Dark Theme With Next.js and MUI
+      </Typography>
     </main>
   );
 }
